fb0afbcfd11da669ce7c314511e71aed35896fb9
BrianReisman <brianreisman@protonmail.com>
2021-02-18 11:35:57 -0500

    initial commit + MVP met

--- src/components/Products.js
+++ src/components/Products.js
@@ -1,20 +1,24 @@
-import React from 'react';
+//*import the ability to subscribe with useContext
+import React, { useContext } from "react";
 
 // Components
-import Product from './Product';
+import Product from "./Product";
+//*import what you'd like to subscribe to that you made
+import { ProductContext } from "../context/ProductContext";
 
-const Products = props => {
-	return (
-		<div className="products-container">
-			{props.products.map(product => (
-				<Product
-					key={product.id}
-					product={product}
-					addItem={props.addItem}
-				/>
-			))}
-		</div>
-	);
+const Products = () => {
+// * marry the two together! What it gives us is a rip in the space time continuum and RETURNS AN OBJECT
+const asdf = useContext(ProductContext)
+const {addItem, products} = asdf;
+console.log(products)
+
+  return (
+    <div className="products-container">
+      {products.map((product) => (
+        <Product key={product.id} product={product} addItem={addItem} />
+      ))}
+    </div>
+  );
 };
 
 export default Products;
